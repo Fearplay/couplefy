fbd6ce860c21c9b3e7bbefbd3a526bb863c22405
Juraj Acsay <juraj.acsay@seznam.cz>
2025-05-28 22:33:25 +0200

    Revert "screenshot were added also readme file was changed"

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:couplefy/app.dart';
 import 'package:couplefy/utils/shared_preferences_utils.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,6 +14,7 @@ void main() async {
   ]);
   // Init of things in Shared Preferences
   await SharedPreferencesUtils.init();
+  GoogleFonts.config.allowRuntimeFetching = false;
   // Run my app
   runApp(MyApp());
 }
