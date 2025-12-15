44a51b497af883a9098c59b41afacec09e07b66a
Juraj Acsay <juraj.acsay@seznam.cz>
2025-12-15 20:34:57 +0100

    Change of holidays + delete of support me button

--- lib/widgets/settings/option_buttons_row.dart
+++ lib/widgets/settings/option_buttons_row.dart
@@ -2,7 +2,6 @@ import 'package:couplefy/l10n/app_localizations.dart';
 import 'package:couplefy/theme/app_text_styles.dart';
 import 'package:couplefy/widgets/settings/options/global_holidays_switch.dart';
 import 'package:couplefy/widgets/settings/options/start_from_zero_switch.dart';
-import 'package:couplefy/widgets/settings/options/support_me_row.dart';
 import 'package:flutter/material.dart';
 import 'package:couplefy/widgets/settings/options/about_app_row.dart';
 import 'package:couplefy/widgets/settings/options/dark_mode_switch.dart';
